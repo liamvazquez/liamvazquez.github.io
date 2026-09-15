c8c09f226b76bec65ca04246fe6406620017cf7a
rasfiiiiii <cosycosy83@gmail.com>
2026-09-16 00:03:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/liam-vazquez/",
+  vite: {
+    base: "/liam-vazquez/",
+  },
 
   tanstackStart: {
     server: { entry: "server" },
